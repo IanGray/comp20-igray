8fc9daaa10147c5bccca1a28d82dd34153d1c17d
Ian Gray <iangray27@gmail.com>
2015-03-11 01:48:39 -0400

    previous commit messed with things

--- messages/lab.js
+++ messages/lab.js
@@ -5,20 +5,16 @@ function parse() {
 	myfile.onreadystatechange = function() {
 		if(myfile.readyState == 4 && myfile.status == 200) {
 			var jsonArr = JSON.parse(myfile.responseText)
-			outputFunc(jsonArr)
+			var output = "";
+  			var i;
+    		for(i = 0; i < arr.length; i++) {
+    		    output += '<p><span class="username">' + arr[i].username +
+    		    '</span>: ' + arr[i].content + '</p>';
+    		}
+    		document.getElementById("messages").innerHTML = output;
+    		//toUpdate = document.getElementById("content")
 		}
 	}
 	myfile.send()
-}
-
-outputFunc(arr) {
-	var output = "";
-  		var i;
-    	for(i = 0; i < arr.length; i++) {
-   		    output += '<p><span class="username">' + arr[i].username +
- 		    '</span>: ' + arr[i].content + '</p>';
-    	}
-   		document.getElementById("messages").innerHTML = output;
-   		toUpdate = document.getElementById("content")
 }
 
