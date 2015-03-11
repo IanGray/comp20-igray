1daff10612c47f7e3a2d2138dee461f424c5554e
Ian Gray <iangray27@gmail.com>
2015-03-11 01:47:39 -0400

    trying 2 funcs again

--- messages/lab.js
+++ messages/lab.js
@@ -4,17 +4,21 @@ function parse() {
 
 	myfile.onreadystatechange = function() {
 		if(myfile.readyState == 4 && myfile.status == 200) {
-			var arr = JSON.parse(myfile.responseText)
-			var output = "";
-  			var i;
-    		for(i = 0; i < arr.length; i++) {
-    		    output += '<p><span class="username">' + arr[i].username +
-    		    '</span>: ' + arr[i].content + '</p>';
-    		}
-    		document.getElementById("messages").innerHTML = output;
-    		//toUpdate = document.getElementById("content")
+			var jsonArr = JSON.parse(myfile.responseText)
+			outputFunc(jsonArr)
 		}
 	}
 	myfile.send()
+}
+
+outputFunc(arr) {
+	var output = "";
+  		var i;
+    	for(i = 0; i < arr.length; i++) {
+   		    output += '<p><span class="username">' + arr[i].username +
+ 		    '</span>: ' + arr[i].content + '</p>';
+    	}
+   		document.getElementById("messages").innerHTML = output;
+   		toUpdate = document.getElementById("content")
 }
 
