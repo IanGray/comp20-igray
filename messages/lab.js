564cc689013381e1e2c6946fb69f845f83edaa12
Ian Gray <iangray27@gmail.com>
2015-03-11 01:29:38 -0400

    merged funcs to try to fix output

--- messages/lab.js
+++ messages/lab.js
@@ -4,8 +4,15 @@ function parse() {
 
 	myfile.onreadystatechange = function() {
 		if(myfile.readyState == 4 && myfile.status == 200) {
-			var jsonArr = JSON.parse(myfile.responseText)
-			outputFunc(jsonArr)
+			var arr = JSON.parse(myfile.responseText)
+			var output = "";
+  			var i;
+    		for(i = 0; i < arr.length; i++) {
+    		    output += '<p>' + arr[i].username + ': ' + 
+        		arr[i].content + '</p>';
+    		}
+    		document.getElementById("messages").innerHTML = output;
+    		toUpdate = document.getElementById("content")
 		}
 	}
 	myfile.send()
