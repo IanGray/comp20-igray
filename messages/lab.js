afc802ac764f99269d9abf5c1c90422877a5ba31
Ian Gray <iangray27@gmail.com>
2015-03-10 23:36:49 -0400

    first try with js

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,21 @@
-// Your JavaScript goes here...
+var myfile - new XMLHttpRequest();
+myfile.open("get,data.json",true)
+
+myfile.onreadystatechange = function() {
+	if(myfile.readyState == 4 && myfile.status == 200) {
+		var jsonArr = JSON.parse(myfile.responseText)
+		outputFunc(jsonArr)
+	}
+}
+
+
+
+function outputFunc(arr) {
+	var output = "";
+    var i;
+    for(i = 0; i < arr.length; i++) {
+        output += '<p>' + arr[i].username + ': ' + 
+        arr[i].content + '</p>';
+    }
+    document.getElementById("messages").innerHTML = output;
+}
